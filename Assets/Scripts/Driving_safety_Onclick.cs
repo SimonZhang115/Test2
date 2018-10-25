3382b7d3a278101569c24f07b2a66f211885bda1
yang1692 <44385820+yang1692@users.noreply.github.com>
2018-10-24 20:52:17 -0500

    1.Construct the main manu interface 2.Construct part of the driving safety interface, including:     I. The Menu Option at the top of the panel     II. The Scroll view 3. Add 3 Scripts trying to realize the function of buttons, including:     I. The PanelInstance script to mark the panels we wanna to jump between.     II. The PanelManager script to provide different functions we will use.     III. The MainMenuManager script to provide the main menu functions     IV. The Driving_safety_onClick script to provide the events when click on the driving safety button. But due to some unknow, the switch of panels doesn't work as I wish.

---
+++ Assets/Scripts/Driving_safety_Onclick.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driving_safety_Onclick : PanelManager
+{
+    public void OnclickOfDrivingSafety()
+    {
+        CloseAllPanels();
+        OpenPanelByName("Driving_safety");
+    }
+}
---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MainMenuManager : PanelManager
+{
+    public string SceneStart = "Main_Menu";
+    public Text CharacterName;
+    public GameObject Preloader;
+    void Start()
+    {
+
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+    public void OpenPreviousPanel()
+    {
+        if (currentPanel && currentPanel.PanelBefore)
+        {
+            CloseAllPanels();
+            Animator anim = currentPanel.PanelBefore.GetComponent<Animator>();
+            if (anim && anim.isActiveAndEnabled)
+            {
+                anim.SetBool("Open", true);
+            }
+            currentPanel.PanelBefore.gameObject.SetActive(true);
+            currentPanel = currentPanel.PanelBefore;
+        }
+    }
+}
